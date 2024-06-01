2f8531592e070206b1f823c5d4b22ece7f60ba4d
Mahesh Pohekar <maheshpohekar25@gmail.com>
2024-06-01 07:36:06 +0530

    base url update in environment.ts and deployment.yaml

--- car_rental_system_frontend/src/environments/environment.ts
+++ car_rental_system_frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
-    API_URL: 'http://localhost:8080' // Your backend API URL for development
+    API_URL: 'backed-service:8080' // Your backend API URL for development
   };
   
